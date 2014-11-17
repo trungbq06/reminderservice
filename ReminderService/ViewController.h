b88051e80a3c606c72934eb07d556bc30258e3c6
Mr Trung <trungbq06@gmail.com>
2014-11-17 11:35:13 +0700

    Initial Commit

---
+++ ReminderService/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReminderService
+//
+//  Created by Mr Trung on 11/17/14.
+//  Copyright (c) 2014 Mr Trung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
