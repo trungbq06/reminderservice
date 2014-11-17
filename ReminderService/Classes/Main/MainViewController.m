9801d353b84edd2df426d6c89c2aff71becd047d
Mr Trung <trungbq06@gmail.com>
2014-11-17 19:32:29 +0700

    First commit

--- ReminderService/Classes/Main/MainViewController.m
+++ ReminderService/Classes/Main/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 Mr Trung. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
